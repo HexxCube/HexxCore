e0d2d475a68f46fc532fe60579cc7243bca2985d
HexxCube <10108510+hexxcube@users.noreply.github.com>
2020-04-17 05:46:13 -0700

    Add .gitignore

--- src/main/java/me/hexxcube/mcplugin/hexxcore/io/FileIO.java
+++ src/main/java/me/hexxcube/mcplugin/hexxcore/io/FileIO.java
@@ -1,5 +1,38 @@
 package me.hexxcube.mcplugin.hexxcore.io;
 
+import java.util.InputMismatchException;
+
+// for obtaining specific data from files
 public class FileIO {
+	
+	private final String fileName;
+	
+	public FileIO(String fileName) {
+		this.fileName = fileName;
+		
+		// check if file exist
+		
+	}
+	
+	public FileIO(String fileName, int var) {
+		this(fileName);
+		
+	}
+	
+	private void createFile() {
+		
+	}
+	
+	public int askForInt(String varName) throws InputMismatchException {
+		
+		
+		return 0;
+	}
+	
+	public String askFor(String varName) {
+		
+		
+		return "";
+	}
 	
 }
